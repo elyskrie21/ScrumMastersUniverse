2c81ec4105fc4163111d1e7eacbfc1c34ec05199
Elyse Kriegel <esamkriegel@gmail.com>
2023-08-03 13:42:37 -0500

    tweaked contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,30 +7,31 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const Contact = () => {
   const form = useRef(null);
-  const [message, setMessage] = useState(""); 
+  const [message, setMessage] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  
+
   const setTextMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(e.target.value)
-  }
+    setMessage(e.target.value);
+  };
 
   const setNewName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
   const setNewEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (form.current) {
       if (!message || !name || !email) {
-        toast.error("Enter all fields")
+        toast.error("Enter all fields");
         return;
       }
+      toast.info("Sending email")
       emailjs
         .sendForm(
           "contact_service",
@@ -41,9 +42,9 @@ export const Contact = () => {
         .then(
           (result) => {
             if (result.status == 200) {
-            toast.success("Email successfully sent!")
+              toast.success("Email successfully sent!");
             } else {
-              toast.error("Email not sent :(")
+              toast.error("Email not sent :(");
             }
           },
           (error) => {
@@ -62,96 +63,79 @@ export const Contact = () => {
           </h1>
         </div>
       </section>
-      <section className="text-gray-600 body-font relative">
-        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-end relative">
-            <iframe
-              className="absolute inset-0"
-              style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
-              title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95523.66026231337!2d-93.68891621762076!3d41.566768974865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87ee99a4c1611ee7%3A0x710028512691e4b2!2sDes%20Moines%2C%20IA!5e0!3m2!1sen!2sus!4v1690868609993!5m2!1sen!2sus"
-              width="100%"
-              height="100%"
-            ></iframe>
-            <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                  EMAIL
-                </h2>
-                <a className="text-purple-500 leading-relaxed">
-                  info@scrummastersoftheuniverse.org
-                </a>
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-                  PHONE
-                </h2>
-                <p className="leading-relaxed">123-456-7890</p>
-              </div>
+      <section className="bg-white dark:bg-gray-900">
+        <div>
+          <ToastContainer />
+        </div>
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            Contact Us
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            Got a question? Want to send feedback? Need more details? Let us
+            know.
+          </p>
+          <form ref={form} onSubmit={sendEmail} className="space-y-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                name="from_name"
+                type="email"
+                id="email"
+                onChange={setNewEmail}
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="name@domain.com"
+                required
+              />
             </div>
-          </div>
-          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Contact Us
-            </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">
-              Need to contact us? Use the form below.
-            </p>
-            <form ref={form} onSubmit={sendEmail}>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  onChange={setNewName}
-                  className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="email"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  onChange={setNewEmail}
-                  className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="message"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  onChange={setTextMessage}
-                  className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-              <button className="text-white bg-primary-600 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
-                Send
-              </button>
-            </form>
-          </div>
-          <div>
-            <ToastContainer />
-          </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                onChange={setNewName}
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                rows={6}
+                onChange={setTextMessage}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Send message
+            </button>
+          </form>
         </div>
       </section>
       <Footer />
     </div>
   );
 };
-
